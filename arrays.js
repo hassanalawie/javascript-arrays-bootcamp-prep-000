f3bf3117b4ea3d003832eec6f8546849f298e4a0
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-17 19:15:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToBeginningOfArray(array[],element)
 
 function destructivelyAddElementToBeginningOfArray(array[],element)
 {
-  a.unshift(b);
+  array.unshift(element);
   return a
 }
