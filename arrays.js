1e6a5086f1bda8debed42a4356fb0e48169acca3
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-17 19:31:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,3 +36,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array;
 }
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
