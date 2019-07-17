c3fe540a6df761496714f1ad41c3228694a3970a
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-17 19:21:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,16 +4,16 @@ var chocolateBars=
   'hundred grand',
   'kitkat',
   'skittles'  
-]
-element='foo'
+];
+element='foo';
 function addElementToBeginningOfArray(array,element)
 {
   c=[element,...array];
-  return c
+  return c;
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element)
 {
   array.unshift(element);
-  return array
+  return array;
 }
